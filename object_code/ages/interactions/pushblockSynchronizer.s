f1b9361250892bb22df67ef397c978af4a50bbeb
Manuel Salgado <manuelpdla@gmail.com>
2025-07-10 13:24:45 +0200

    Segundo piso de la mazmorra 5 entendido y comentado el código relevante.

--- object_code/ages/interactions/pushblockSynchronizer.s
+++ object_code/ages/interactions/pushblockSynchronizer.s
@@ -11,14 +11,16 @@ interactionCodebd:
 
 @state1:
 	; Wait for a block to be pushed
-	ld a,(w1ReservedInteraction1.enabled)
+	ld a,(w1ReservedInteraction1.enabled) ;cuando un bloque está siendo empujado, copia su información en w1ReservedInteraction1, poniendo además
+	; w1ReservedInteraction1.enabled a 1, indicando que está activo.
 	or a
-	ret z
+	ret z ;si no hay un bloque siendo empujado, sale
 
+	;si sí hay un bloque siendo empujado sigue con el código
 	ld a,(w1ReservedInteraction1.var31) ; Tile index of block being pushed
-	ldh (<hFF8B),a
+	ldh (<hFF8B),a ;guarda el tileindex empujado en hFF8B
 	call findTileInRoom
-	jr nz,@incState
+	jr nz,@incState ;si no encuentra otro tileindex igual en la sala, vuelve al principio de este state1.
 
 	; Found another tile of the same type; push it, then search for more tiles of that type
 	call @pushBlockAt
@@ -39,15 +41,16 @@ interactionCodebd:
 	ret
 
 ;;
-; @param	hl	Position of block to push in wRoomLayuut
+; @param	hl	Position of block to push in wRoomLayout
 ; @param	hFF8B	Index of tile to push
 @pushBlockAt:
-	push hl
+	push hl ;guarda en la pila la posición original del bloque a mover.
+
 	ldh a,(<hFF8B)
 	cp TILEINDEX_SOMARIA_BLOCK
-	jr z,@return
+	jr z,@return ;si el tileindex es un somaria block, no lo empuja, es un tileindex especial
 
-	ld a,l
+	ld a,l 
 	ldh (<hFF8D),a
 	ld h,d
 	ld l,Interaction.yh
